d2afe770065603657193845772c7ea4bd095e282
Yannis Tsopokis <tsopokis@users.noreply.github.com>
2022-11-14 14:35:14 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,25 @@
+input.onButtonPressed(Button.A, function () {
+    music.stopAllSounds()
+    pixel_array.range(0, 2).showColor(neopixel.colors(NeoPixelColors.Blue))
+    pixel_array.range(3, 2).showColor(neopixel.colors(NeoPixelColors.Blue))
+    pins.servoWritePin(AnalogPin.P1, 0)
+    pins.servoWritePin(AnalogPin.P2, 180)
+})
+input.onButtonPressed(Button.AB, function () {
+    music.stopAllSounds()
+    pixel_array.range(0, 2).showColor(neopixel.colors(NeoPixelColors.Red))
+    pixel_array.range(3, 2).showColor(neopixel.colors(NeoPixelColors.Red))
+    pins.servoWritePin(AnalogPin.P1, 90)
+    pins.servoWritePin(AnalogPin.P2, 90)
+})
+input.onButtonPressed(Button.B, function () {
+    music.ringTone(262)
+    pixel_array.range(0, 2).showColor(neopixel.colors(NeoPixelColors.White))
+    pixel_array.range(3, 2).showColor(neopixel.colors(NeoPixelColors.White))
+    pins.servoWritePin(AnalogPin.P1, 180)
+    pins.servoWritePin(AnalogPin.P2, 0)
+})
+let pixel_array: neopixel.Strip = null
+pixel_array = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
+basic.showIcon(IconNames.Target)
+music.setBuiltInSpeakerEnabled(true)
